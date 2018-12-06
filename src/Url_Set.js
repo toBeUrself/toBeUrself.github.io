da78686d70947b95abe6eb7f24acda6c9cc8fa8d
tim <liuzeyunqz@163.com>
2018-12-06 22:52:56 +0800

    update access token

--- src/Url_Set.js
+++ src/Url_Set.js
@@ -4,5 +4,5 @@ export var UrlSet = {
     GithubAuth: 'https://github.com/login/oauth/authorize?client_id=35f234d9b37bdaaa11dd',
     GithubAccess: 'https://github.com/login/oauth/access_token?client_id=35f234d9b37bdaaa11dd&client_secret=af87943040e72403431ebca2a3335411d0fee9fa&code=',
     GithubUserInfo: 'https://api.github.com/user?',
-    GithubInfo: 'access_token=bd02725eaf9984d157eda273fac9ac49767a82d8'
+    GithubInfo: 'access_token=bd729226a2294222a3ea5fb5a169e8ae2a8fc4e2'
 }
