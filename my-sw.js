46459b7dddd25a704d959073d4e99f98aa25d6a0
Tim <44401117+toBeUrself@users.noreply.github.com>
2019-04-17 16:47:56 +0800

    Update my-sw.js

--- my-sw.js
+++ my-sw.js
@@ -1,5 +1,24 @@
 'use strict';
 
+var cacheName = 'tim-blog';
+var filesToCache = [
+  '/*.css',
+  '/*.js',
+  '/img/*',
+  '/index.html’,
+  '/manifest.json’,
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
 self.addEventListener('install', event => {
 
 });
