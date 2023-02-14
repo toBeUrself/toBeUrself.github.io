19fcaa44453d2ad03284f901a8986fc3b6c8e128
Tim Liu <liuzeyunqz@163.com>
2023-02-14 22:04:38 +0800

    feat: auto play

--- summary/appjs/src/App.js
+++ summary/appjs/src/App.js
@@ -98,10 +98,43 @@ function App() {
 
   useEffect(() => {
     const elementAudio = document.getElementById('audio');
+    audioAutoPlay();
+    autoPlayMusic();
 
-    window.WeixinJSBridge && window.WeixinJSBridge.invoke('getNetworkType', {}, () => {
-      elementAudio.play();
-    }, false);
+    function audioAutoPlay() {
+      var audio = document.getElementById('audio');
+      audio.play();
+      document.addEventListener("WeixinJSBridgeReady", function () {
+          audio.play();
+      }, false);
+    }
+    // 音乐播放
+    function autoPlayMusic() {
+        // 自动播放音乐效果，解决浏览器或者APP自动播放问题
+        function musicInBrowserHandler() {
+            musicPlay(true);
+            document.body.removeEventListener('touchstart', musicInBrowserHandler);
+        }
+        document.body.addEventListener('touchstart', musicInBrowserHandler);
+        // 自动播放音乐效果，解决微信自动播放问题
+        function musicInWeixinHandler() {
+            musicPlay(true);
+            document.addEventListener("WeixinJSBridgeReady", function () {
+                musicPlay(true);
+            }, false);
+            document.removeEventListener('DOMContentLoaded', musicInWeixinHandler);
+        }
+        document.addEventListener('DOMContentLoaded', musicInWeixinHandler);
+    }
+    function musicPlay(isPlay) {
+        var media = document.querySelector('#audio');
+        if (isPlay && media.paused) {
+            media.play();
+        }
+        if (!isPlay && !media.paused) {
+            media.pause();
+        }
+    }
   }, []);
 
   return (
@@ -110,7 +143,7 @@ function App() {
         <div dangerouslySetInnerHTML={{ __html: element[0]}} />
       ) : <span id="typed"></span>}
 
-      <audio id="audio" src="https://www.xzmp3.com/down/19adb03e470b.mp3" autoPlay loop></audio>
+      <audio id="audio" style={{display: 'none', height: 0}} preload='auto' src="https://www.xzmp3.com/down/19adb03e470b.mp3" loop></audio>
     </div>
   );
 }
